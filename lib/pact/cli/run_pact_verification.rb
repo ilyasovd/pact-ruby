5aba022ffd3d8de3fdeb38c05558973d907c028f
Denis Ilyasov <ilyasovd@users.noreply.github.com>
2023-11-17 01:49:54 +0500

    Update run_pact_verification.rb

--- lib/pact/cli/run_pact_verification.rb
+++ lib/pact/cli/run_pact_verification.rb
@@ -52,7 +52,8 @@ module Pact
         else
           run_with_configured_pacts_from_pact_helper
         end
-        exit exit_code
+        # exit exit_code
+        Process.exit!(exit_code)
       end
 
       def run_with_pact_url_string
